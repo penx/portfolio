affbfa465387f80acae7324898790da4b331631c
Alasdair McLeay <alasdair.mcleay@me.com>
2016-11-27 21:46:36 +0000

    basic use of state on article component

--- source/shared/components/Article.js
+++ source/shared/components/Article.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class ArticleComponent extends React.Component {
+
+  state = {
+    title: "Article Title",
+    content: "Content"
+  }
+
   render() {
+    const {title} = this.state;
     return (
       <div>
-        <h2>Article</h2>
+        <h2>{title}</h2>
         <p>Go to <Link to='/project/example-slug'>a project</Link></p>
         {this.props.children}
       </div>
