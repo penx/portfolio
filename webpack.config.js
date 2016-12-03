cc150501d6411d946ea73c0d3a787f6f04f3b240
Alasdair McLeay <alasdair.mcleay@me.com>
2016-12-03 19:26:09 +0000

    production build of react

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-'use strict';
-
-let path = require('path');
-let webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 // TODO: dist and dev builds, eslint, live reload, sass
 
@@ -9,21 +7,24 @@ module.exports = {
   entry: path.join(process.cwd(), 'source/client/app.js'),
   output: {
     path: './dist/client/',
-    filename: 'build.js'
+    filename: 'build.js',
   },
   devtool: 'sourcemap',
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
   ],
   module: {
     loaders: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel'
-      }
-    ]
+        loader: 'babel',
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
-  }
-};
+    extensions: ['', '.js', '.jsx'],
+  },
+}
